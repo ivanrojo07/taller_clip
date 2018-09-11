4ac6a4ca1cd1adce8c2358cb659cbe122be3b418
servin <tarzanboycraft@hotmail.com>
2018-09-11 13:09:35 -0500

    orden modelo

--- app/Producto.php
+++ app/Producto.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    protected $table = 'orden';
+    protected $fillable=['id',
+    					 'nombre',
+						 'fecha',
+                         'descripcion'];
 }
