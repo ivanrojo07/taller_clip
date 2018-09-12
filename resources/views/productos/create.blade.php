841d562dffc4b42a645029dd08bb53da6e0b46f7
servin <tarzanboycraft@hotmail.com>
2018-09-11 23:35:31 -0500

    selects

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -7,6 +7,50 @@
 	<form action="" id="explosionadoForm"></form>
 
 			<div class="row">
+
+				<div class="col-12 mb 2">
+					<h4>Datos Órden</h4>
+				</div>
+
+				<div class="col-6 mb-2">
+					<!--espe-->
+					<div class="input-group ">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="basic-addon3">Nombre</span>
+						</div>
+						<input type="text" class="form-control" id="nombre" aria-describedby="basic-addon3" form="explosionadoForm">
+					</div>
+				</div>
+				<div class="col-6 mb-2">
+					<!--espe-->
+					<div class="input-group ">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="basic-addon3">Fecha</span>
+						</div>
+						<input type="date" class="form-control" id="fecha" aria-describedby="basic-addon3" form="explosionadoForm" readonly>
+					</div>
+				</div>
+				<div class="col-6 mb-2">
+					<!--espe-->
+					<div class="input-group ">
+						<div class="input-group-prepend">
+							<span class="input-group-text" id="basic-addon3">ID</span>
+						</div>
+						<input type="text" class="form-control" id="id" aria-describedby="basic-addon3" form="explosionadoForm" readonly>
+					</div>
+				</div>
+				<div class="col-6 mb-2">
+				    <label for="exampleFormControlTextarea1">Descripción</label>
+					<textarea class="form-control" id="descripcion" rows="3"></textarea>
+				</div>
+
+	
+
+				<div class="col-12 mb 2">
+					<h4>Buscar Materiales</h4>
+				</div>
+
+
 				<div class="col-6 mb-2">
 					<!--Tipo de mat-->
 					<div class="input-group">
@@ -14,9 +58,10 @@
 							<label class="input-group-text" for="inputGroupSelect01">Sección</label>
 						</div>
 						<select class="custom-select" id="tipoMaterial" form="explosionadoForm">
-							<option value="1">opción</option>
-							<option value="2">opción</option>
-							<option value="3">opción</option>
+							<option value="montaje">Montaje</option>
+							<option value="proteccion">Portección</option>
+							<option value="marcos">Marcos y Bastidores</option>
+							<option value="marialuisa">Maria Luisa</option>
 						</select>
 					</div>
 				</div>
@@ -40,11 +85,11 @@
 							<span class="input-group-text" id="basic-addon3">Alto</span>
 						</div>
 						<input type="text" class="form-control" id="alto" aria-describedby="basic-addon3" form="explosionadoForm">
-						<select class="custom-select" id="inputGroupSelect01">
+						<select class="custom-select" id="unidades1">
 							<option selected>Unidades...</option>
-							<option value="1">m</option>
-							<option value="2">cm</option>
-							<option value="3">mm</option>
+							<option value="m">m</option>
+							<option value="cm">cm</option>
+							<option value=mm>mm</option>
 						</select>
 					</div>
 				</div>
@@ -55,11 +100,11 @@
 							<span class="input-group-text" id="basic-addon3">Ancho</span>
 						</div>
 						<input type="text" class="form-control" id="ancho" aria-describedby="basic-addon3" form="explosionadoForm">
-						<select class="custom-select" id="inputGroupSelect01">
+						<select class="custom-select" id="unidades2">
 							<option selected>Unidades...</option>
-							<option value="1">m</option>
-							<option value="2">cm</option>
-							<option value="3">mm</option>
+							<option value="m">m</option>
+							<option value="cm">cm</option>
+							<option value="mm">mm</option>
 						</select>
 					</div>
 				</div>
@@ -81,37 +126,7 @@
 						<input type="text" class="form-control" id="espesor" aria-describedby="basic-addon3" form="explosionadoForm">
 					</div>
 				</div>
-				<div class="col-6 mb-2">
-					<!--espe-->
-					<div class="input-group ">
-						<div class="input-group-prepend">
-							<span class="input-group-text" id="basic-addon3">Nombre</span>
-						</div>
-						<input type="text" class="form-control" id="nombre" aria-describedby="basic-addon3" form="explosionadoForm">
-					</div>
-				</div>
-				<div class="col-6 mb-2">
-					<!--espe-->
-					<div class="input-group ">
-						<div class="input-group-prepend">
-							<span class="input-group-text" id="basic-addon3">Fecha</span>
-						</div>
-						<input type="text" class="form-control" id="fecha" aria-describedby="basic-addon3" form="explosionadoForm">
-					</div>
-				</div>
-				<div class="col-6 mb-2">
-					<!--espe-->
-					<div class="input-group ">
-						<div class="input-group-prepend">
-							<span class="input-group-text" id="basic-addon3">ID</span>
-						</div>
-						<input type="text" class="form-control" id="id" aria-describedby="basic-addon3" form="explosionadoForm" readonly>
-					</div>
-				</div>
-				<div class="col-6 mb-2">
-				    <label for="exampleFormControlTextarea1">Descripción</label>
-					<textarea class="form-control" id="descripcion" rows="3"></textarea>
-				</div>
+				
 				<div class="col-6 my-3">
 					<button type="button" class="btn btn-warning btn-lg btn-block">Buscar</button>
 				</div>
@@ -179,7 +194,21 @@
 			<a href="{{route('cotizacion')}}" class="btn btn-warning btn-lg btn-block" role="button" aria-pressed="true">Cotizar</a>
 
 	<script>
-		
+		$(document).ready(function(){
+			var now = new Date();
+			var day = ("0" + now.getDate()).slice(-2);
+			var month = ("0" + (now.getMonth() + 1)).slice(-2);
+			var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+			$('#fecha').val(today);
+		});
+		$('#unidades1').on('change', function(){
+			$('#unidades2').val($('#unidades1').val());
+
+		});
+		$('#unidades2').on('change', function(){
+			$('#unidades1').val($('#unidades2').val());
+
+		});
 	</script>
 </div>
 
